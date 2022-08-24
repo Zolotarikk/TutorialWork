3af2b6a9c5d63bfed2883e10a95cc34ffa6a0070
Vitalik <vitalik.zolotar09@gmail.com>
2022-08-24 14:46:53 +0300

    Add function

---
+++ script.js
@@ -0,0 +1,11 @@
+function NumberOf(a){
+var number = 0;
+    for(let i = 0;i<=a.length;i++){
+        if(a[i]=='a'){
+            number++;
+        }
+    }return number;
+}
+var str = prompt('Enter your line');
+let a = NumberOf(str);
+console.log(a);
